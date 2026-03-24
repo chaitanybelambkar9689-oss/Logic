176dee3b9ec5e45c14757d4c6a2a0c557a627b4d
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-03-24 21:25:11 +0530

    Add files via upload

---
+++ Assignment_46/program226.c
@@ -0,0 +1,129 @@
+
+/*
+Input Linked list : |11|-> |22|-> |28|-> |101|-> |111|-> |11|->|NULL|
+Total number of nodes are : 6
+101     111
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+
+void InsertFirst(PPNODE First,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        newn -> next = *First;
+        *First = newn;
+    }
+
+}
+
+void InsertLast(PPNODE First ,int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn -> data = no;
+    newn ->next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        temp = *First ;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+
+}
+
+void Display(PNODE first)
+{
+    printf("Input Linked list :");
+    while(first != NULL)
+    {
+        printf(" |%d|->",first ->data);
+        first = first -> next;
+    }
+    printf("|NULL|\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while((first)  != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+    return iCount;
+}
+
+void DisplayGreater(PNODE first,int no)
+{
+   int iPos = 1;
+   int iCount = 0;
+
+    while(first  != NULL)
+    {
+        if((first -> data) >  no)
+        {
+            printf("%d \t",first -> data);
+        }
+        first = first -> next ;
+    }
+    printf("\n");
+  
+}
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+    bool bRet =  false;
+
+    InsertLast(&head,11);
+    InsertLast(&head,22);
+    InsertLast(&head,28);
+    InsertLast(&head,101);
+    InsertLast(&head,111);
+    InsertLast(&head,11);
+
+    Display(head);
+    iRet =  Count(head);
+    printf("Total number of nodes are : %d\n",iRet);
+
+    DisplayGreater(head,28);
+
+   
+
+    return 0;
+}
---
+++ Assignment_46/program229.c
@@ -0,0 +1,142 @@
+
+/*
+All the nodes before nodes replacement
+Input Linked list : |11|-> |22|-> |-28|-> |-101|-> |111|-> |11|->|NULL|
+Total number of nodes are : 6
+
+All the nodes display after replacement
+Input Linked list : |11|-> |22|-> |0|-> |0|-> |111|-> |11|->|NULL|
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+
+void InsertFirst(PPNODE First,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        newn -> next = *First;
+        *First = newn;
+    }
+
+}
+
+void InsertLast(PPNODE First ,int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn -> data = no;
+    newn ->next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        temp = *First ;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+
+}
+
+void Display(PNODE first)
+{
+    printf("Input Linked list :");
+    while(first != NULL)
+    {
+        printf(" |%d|->",first ->data);
+        first = first -> next;
+    }
+    printf("|NULL|\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while((first)  != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+    return iCount;
+}
+
+void ReplacementNegative(PPNODE first)
+{
+   int iPos = 1;
+   int iCount = 0;
+
+   PNODE temp = NULL;
+
+   temp = *first;
+
+    while(temp  != NULL)
+    {
+        if(temp -> data <  0)
+        {
+            temp -> data = 0;
+        }
+
+        temp = temp -> next ;
+    }
+    printf("\n");
+  
+}
+
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+    bool bRet =  false;
+
+    InsertLast(&head,11);
+    InsertLast(&head,22);
+    InsertLast(&head,-28);
+    InsertLast(&head,-101);
+    InsertLast(&head,111);
+    InsertLast(&head,11);
+
+    printf("All the nodes display nodes replacement\n");
+    Display(head);
+    iRet =  Count(head);
+    printf("Total number of nodes are : %d\n",iRet);
+
+    ReplacementNegative(&head);
+
+    printf("All the nodes display after replacement\n");
+    Display(head);   
+
+
+    return 0;
+}
